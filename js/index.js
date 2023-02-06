4f2ba0687792e4fe19d7aedde7cda329b4cff0f5
Davy <nikitayashin3@gmail.com>
2023-02-06 23:10:02 +0300

    made slider script

--- js/index.js
+++ js/index.js
@@ -7,7 +7,69 @@ const removePreload = elem => {
 }
 
 const startSlider = (slider) => {
-    
+    const sliderItems = document.querySelectorAll('.slider__item')
+    const sliderList = document.querySelector('.slider__list')
+    const btnPrevSlide = document.querySelector('.slider__arrow_left')
+    const btnNextSlide = document.querySelector('.slider__arrow_right')
+
+    let activeSlide = 1
+    let position = 0
+
+    const checkSlider = () => {
+        activeSlide + 2 === sliderItems.length 
+        && document.documentElement.offsetWidth > 560
+        || activeSlide === sliderItems.length
+
+        ? btnNextSlide.style.display = 'none'
+        : btnNextSlide.style.display = ''
+
+        activeSlide === 1
+        ? btnPrevSlide.style.display = 'none'
+        : btnPrevSlide.style.display = ''
+
+        }
+
+    checkSlider()
+
+    const nextSlide = () => {
+        sliderItems[activeSlide]?.classList.remove('slider__item_active')
+
+        position = sliderItems[0].clientWidth * activeSlide
+        sliderList.style.transform = `translateX(-${position}px)`
+        activeSlide += 1
+
+        sliderItems[activeSlide]?.classList.add('slider__item_active')
+
+        checkSlider()
+    }
+
+    const prevSlide = () => {
+        sliderItems[activeSlide]?.classList.remove('slider__item_active')
+
+        position = -sliderItems[0].clientWidth * (activeSlide - 2)
+        sliderList.style.transform = `translateX(${position}px)`
+        activeSlide -= 1
+
+        sliderItems[activeSlide]?.classList.add('slider__item_active')
+
+        checkSlider()
+    }
+
+    btnPrevSlide.addEventListener('click', prevSlide)
+    btnNextSlide.addEventListener('click', nextSlide)
+
+    window.addEventListener('resize', () => {
+        if(activeSlide + 2 > sliderItems.length &&
+        document.documentElement.offsetWidth > 560) {
+        activeSlide = sliderItems.length - 2
+        sliderItems[activeSlide]?.classList.add('slider__item_active')
+        }
+
+        position = -sliderItems[0].clientWidth * (activeSlide - 1)
+        sliderList.style.transform = `translateX(${position}px)`
+        checkSlider()
+    })
+
 }
 
 const initSlider = () => {

6a9cf1ead88960724388b232ce0e88bd9346d225
Davy <nikitayashin3@gmail.com>
2023-02-08 16:33:05 +0300

    add render functions for reserve section

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://educated-kindly-paddleboat.glitch.me/api';
+const API_URL = 'https://educated-kindly-paddleboat.glitch.me/';
 
 const addPreload = elem => {
     elem.classList.add('preload')
@@ -8,7 +8,7 @@ const removePreload = elem => {
     elem.classList.remove('preload')
 }
 
-const startSlider = (slider) => {
+const startSlider = () => {
     const sliderItems = document.querySelectorAll('.slider__item')
     const sliderList = document.querySelector('.slider__list')
     const btnPrevSlide = document.querySelector('.slider__arrow_left')
@@ -90,6 +90,18 @@ const initSlider = () => {
     })
 }
 
+const addDisabled = (arr) => {
+    arr.forEach(el => {
+        el.disabled = true
+    })
+}
+
+const removeDisabled = (arr) => {
+    arr.forEach(el => {
+        el.disabled = false
+    })
+}
+
 const renderPrice = (wrapper, data) => {
     data.forEach(item => {
         const priceItem = document.createElement('li')
@@ -104,25 +116,119 @@ const renderPrice = (wrapper, data) => {
     });
 }
 
+const renderService = (wrapper, data) => {
+    const labels = data.map(item => {
+        const label = document.createElement('label')
+        label.classList.add('radio')
+        label.innerHTML = `
+        <input class="radio__input" type="radio" name="service" value="${item.id}">
+        <span class="radio__label">${item.name}</span>
+        `
+
+        return label
+    })
+
+    wrapper.append(...labels)
+}
+
+const renderSpec = (wrapper, data) => {
+    const labels = data.map(item => {
+        const label = document.createElement('label')
+        label.classList.add('radio')
+        label.innerHTML = `
+        <input class="radio__input" type="radio" name="spec" value="${item.id}">
+        <span class="radio__label radio__label_spec" style="--bg-image:url(${API_URL}${item.img})">${item.name}</span>
+        `
+
+        return label
+    })
+
+    wrapper.append(...labels)
+}
+
+const renderMonth = (wrapper, data) => {
+    const labels = data.map(item => {
+        const label = document.createElement('label')
+        label.classList.add('radio')
+        label.innerHTML = `
+        <input class="radio__input" type="radio" name="month" value="${item}">
+        <span class="radio__label">${new Intl.DateTimeFormat('ru-RU', {
+            month: 'long'
+        }).format(new Date(item))}</span>
+        `
+
+        return label
+    })
+
+    wrapper.append(...labels)
+}
+
 const initService = () => {
     const priceList = document.querySelector('.price__list')
+    const fieldService = document.querySelector('.reserve__fieldset_service')
+
     priceList.textContent = ''
+    fieldService.innerHTML = '<legend class="reserve__legend">Услуга</legend>'
 
     addPreload(priceList)
+    addPreload(fieldService)
 
-    fetch(API_URL)
+    fetch(`${API_URL}/api`)
     .then(response => response.json())
     .then(data => {
         renderPrice(priceList, data)
         removePreload(priceList)
         return data
     })
+    .then(data => {
+        renderService(fieldService, data)
+        removePreload(fieldService)
+    })
  
+}
+
+const initReserve = () => {
+    const reserveForm = document.querySelector('.reserve__form')
+    const {fieldspec, fielddate, fieldmonth, fieldday, fieldtime, btn} = reserveForm
+
+    addDisabled([fieldspec, fielddate, fieldmonth, fieldday, fieldtime, btn])
+
+    reserveForm.addEventListener('change', async event => {
+        const target = event.target
+
+        if (target.name === 'service') {
+            addDisabled([fieldspec, fielddate, fieldmonth, fieldday, fieldtime, btn])
+
+            fieldspec.innerHTML = '<legend class="reserve__legend">Специалист</legend>'
+            addPreload(fieldspec)
+
+            const response = await fetch(`${API_URL}/api/?service=${target.value}`)
+            const data = await response.json()
+
+            renderSpec(fieldspec, data)
+            removePreload(fieldspec)
+            removeDisabled([fieldspec])
+        }
+
+        if (target.name === 'spec') {
+            addDisabled([fielddate, fieldmonth, fieldday, fieldtime, btn])
+
+            addPreload(fieldmonth)
+
+            const response = await fetch(`${API_URL}/api/?spec=${target.value}`)
+            const data = await response.json()
+
+            renderMonth(fieldmonth, data)
+            removePreload(fieldmonth)
+            removeDisabled([fielddate, fieldmonth])
+        }
+    })
 }
 
 const init = () => {
     initSlider()
     initService()
+    initReserve()
 }
 
 init()
